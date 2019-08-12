f06951cc8b3f16883deffda2ecc7ea6095345ea1
ATHeart <b.angelthink@gmail.com>
2019-08-12 20:28:30 +0900

    Added ImageView
    - added Images
    - added buttons and action
      - show prev image at clicked prev button
      - show next image at clicked next button

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    let imgList: [String] = [
+        "mixed rice-1",
+        "nudle",
+        "roast meat",
+        "sashimi",
+        "steak",
+        "sushi"
+    ]
+    var currentIdx: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentIdx = 0
+        setImage(dir: 0)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPrev(_ sender: UIButton) {
+        setImage(dir: -1)
+    }
+    
+    @IBAction func btnNext(_ sender: UIButton) {
+        setImage(dir: 1)
+    }
+    
+    func setImage(dir: Int) -> Void {
+        currentIdx += dir
+        if(currentIdx < 0) {
+            currentIdx = imgList.count + currentIdx
+        } else if(currentIdx > imgList.count - 1) {
+            currentIdx -= imgList.count
+        }
+        imgView.image = UIImage(named: imgList[currentIdx])
+    }
 }
 
